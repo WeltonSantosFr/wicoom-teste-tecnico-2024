94cb29f268662cb481238f2ecc0a2ff010eef4de
welton <weltonsantosfr@gmail.com>
2024-04-19 23:20:28 -0300

    feat: make svgs lighter

--- src/components/carousel/index.tsx
+++ src/components/carousel/index.tsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { BsChevronLeft, BsChevronRight } from "react-icons/bs";
+
 
 export default function Carousel() {
     const [card, setCard] = useState(0)
@@ -29,30 +29,37 @@ export default function Carousel() {
     return (
 
         <div className='absolute inset-0 mx-auto z-50 w-11/12 h-full flex items-center justify-between'>
-            <button onClick={prevCard} className='bg-white rounded-full w-10 h-10 flex items-center justify-center'><BsChevronLeft className='w-6 h-6 text-dark-grey' /></button>
+            <button onClick={prevCard} className='bg-white rounded-full w-10 h-10 flex items-center justify-center'>
+                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none">
+                    <path stroke="#4F4F4F" stroke-linecap="round" stroke-linejoin="round" d="m15 6-5.787 5.787a.3.3 0 0 0 0 .426L15 18" />
+                </svg>
+            </button>
             {slides.map((slide, index) => {
                 return (
-                    <div className={`w-[412px] h-[454px] bg-white flex flex-col relative ${card !== index ? 'hidden': null}`} key={slide.img}>
+                    <div className={`w-[412px] h-[454px] bg-white flex flex-col relative ${card !== index ? 'hidden' : null}`} key={slide.img}>
                         <div className='w-11/12 min-h-[64px] flex items-center mx-auto justify-between'>
-                            <span className='text-dark-grey text-2xl'>#SuperBazar</span>
+                            <span className='text-dark-grey font-normal text-2xl'>#SuperBazar</span>
                             <span className='text-dark-pink font-normal text-sm'>até <span className='text-pink font-bold text-2xl'>50%OFF</span></span>
                         </div>
                         <div className='w-full h-full'>
-                            <img src={slide.img} alt={slide.alt}className='h-[390px] w-full' />
+                            <img src={slide.img} alt={slide.alt} className='h-[390px] w-full' />
                         </div>
-                            <button className='bg-pink w-[156px] h-[48px] absolute -bottom-5 text-white text-base self-center'>Conferir</button>
-                    
-                            <div className="w-[48px] h-[8px] absolute -bottom-24 self-center flex gap-2">
-                                {slides.map((_, index) => {
-                                    return (
-                                        <button key={index} onClick={() => {setCard(index)}} className={`${card === index ? 'bg-pink w-[16px] h-[8px]' : 'bg-black-3 w-[8px] h-[8px]' }`}></button>
-                                    )
-                                })}
-                            </div>
+                        <button className='bg-pink w-[156px] h-[48px] absolute -bottom-5 text-white text-base font-normal self-center'>Conferir</button>
+                        <div className="w-[48px] h-[8px] absolute -bottom-24 self-center flex gap-2">
+                            {slides.map((_, index) => {
+                                return (
+                                    <button key={index} onClick={() => { setCard(index) }} className={` ${card === index ? 'bg-pink w-[16px] h-[8px]' : 'bg-black-3 w-[8px] h-[8px]'}`}></button>
+                                )
+                            })}
+                        </div>
                     </div>
                 )
             })}
-            <button onClick={nextCard} className='bg-white rounded-full w-10 h-10 flex items-center justify-center relative'><BsChevronRight className='w-6 h-6 text-dark-grey' /></button>
+            <button onClick={nextCard} className='bg-white rounded-full w-10 h-10 flex items-center justify-center relative'>
+                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none">
+                    <path stroke="#4F4F4F" stroke-linecap="round" stroke-linejoin="round" d="m9 6 5.787 5.787a.302.302 0 0 1 .066.328.302.302 0 0 1-.066.098L9 18" />
+                </svg>
+            </button>
         </div>
 
     )
